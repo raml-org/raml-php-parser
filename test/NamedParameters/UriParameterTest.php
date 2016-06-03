c4501768277fa6fe8e27db7f06642f3ad5e6fd5c
Michał Tatarynowicz <tatarynowicz@gmail.com>
2016-06-03 14:10:39 +0200

    Allow for using regex uri parameters.

--- test/NamedParameters/UriParameterTest.php
+++ test/NamedParameters/UriParameterTest.php
@@ -37,4 +37,27 @@ RAML;
         $resource = $apiDef->getResourceByUri('/user/1');
         $this->assertInstanceOf('\Raml\Resource', $resource);
     }
+
+    /** @test */
+    public function shouldCorrectlyParseRegexUriParameters()
+    {
+        $raml = <<<RAML
+#%RAML 0.8
+title: User API
+version: 1.2
+/user:
+  /{userName}:
+    displayName: Get a user by name
+    uriParameters:
+      userName:
+        type: string
+        pattern: ^[a-z0-9]+$
+    get:
+      displayName: retrieve a user's picture by user name
+RAML;
+
+        $apiDef = $this->parser->parseFromString($raml, '');
+        $resource = $apiDef->getResourceByUri('/user/alec');
+        $this->assertInstanceOf('\Raml\Resource', $resource);
+    }
 }
